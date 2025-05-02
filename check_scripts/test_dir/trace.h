4c254f25af559ba077f51450d98913ba5edf11c2
Sujan Jain <119590122+jainsujan11@users.noreply.github.com>
2025-05-02 17:02:28 +0530

    Delete check_scripts directory

--- check_scripts/test_dir/trace.h
+++
@@ -1,9 +0,0 @@
-// trace.h
-#ifndef TRACE_H
-#define TRACE_H
-
-extern int state;
-extern int syscall_counts[];
-
-#endif
-
--- check_scripts/test_dir/user_add.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "user.h"
-
-int main(void) {
-  int result = add(3, 5);
-  printf(1, "3 + 5 = %d\n", result);
-  exit();
-}
-
